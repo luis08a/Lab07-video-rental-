fd2b613cf8e74a2feeca5f3a937e5725802c5028
UroBs17 <michael.preciado@mail.escuelaing.edu.co>
2018-10-04 08:53:04 -0500

    inicialización

---
+++ lab6/MyBatis_Introduction_VideoRental/src/main/java/edu/eci/pdsw/sampleprj/dao/mybatis/mappers/ClienteMapper.java
@@ -0,0 +1,35 @@
+package edu.eci.pdsw.sampleprj.dao.mybatis.mappers;
+
+import edu.eci.pdsw.samples.entities.Cliente;
+import java.util.Date;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ *
+ * @author 2106913
+ */
+public interface ClienteMapper {
+    
+	public Cliente consultarCliente(@Param("idcli") int id);
+    
+    /**
+     * Registrar un nuevo item rentado asociado al cliente identificado
+     * con 'idc' y relacionado con el item identificado con 'idi'
+     * @param id
+     * @param idit
+     * @param fechainicio
+     * @param fechafin 
+     */
+    public void agregarItemRentadoACliente(@Param("idc") int idc, 
+    		@Param("idi") int idi, 
+    		@Param("fini") Date fechainicio,
+    		@Param("ffin") Date fechafin);
+
+    /**
+     * Consultar todos los clientes
+     * @return 
+     */
+    public List<Cliente> consultarClientes();
+    
+}
---
+++ lab6/MyBatis_Introduction_VideoRental/src/main/java/edu/eci/pdsw/samples/services/client/MyBatisExample.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2015 hcadavid
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package edu.eci.pdsw.samples.services.client;
+
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import edu.eci.pdsw.sampleprj.dao.mybatis.mappers.ClienteMapper;
+import edu.eci.pdsw.samples.entities.Cliente;
+
+/**
+ *
+ * @author hcadavid
+ */
+public class MyBatisExample {
+
+    /**
+     * Método que construye una fábrica de sesiones de MyBatis a partir del
+     * archivo de configuración ubicado en src/main/resources
+     *
+     * @return instancia de SQLSessionFactory
+     */
+    public static SqlSessionFactory getSqlSessionFactory() {
+        SqlSessionFactory sqlSessionFactory = null;
+        if (sqlSessionFactory == null) {
+            InputStream inputStream;
+            try {
+                inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+                sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+            } catch (IOException e) {
+                throw new RuntimeException(e.getCause());
+            }
+        }
+        return sqlSessionFactory;
+    }
+
+    /**
+     * Programa principal de ejempo de uso de MyBATIS
+     * @param args
+     * @throws SQLException 
+     */
+    public static void main(String args[]) throws SQLException {
+        SqlSessionFactory sessionfact = getSqlSessionFactory();
+
+        SqlSession sqlss = sessionfact.openSession();
+
+        
+        //Crear el mapper y usarlo: 
+        //ClienteMapper cm=sqlss.getMapper(ClienteMapper.class)
+        //cm...
+        ClienteMapper cm=sqlss.getMapper(ClienteMapper.class);
+        List<Cliente> cc=cm.consultarClientes();
+        for(Cliente c:cc) {
+        	System.out.println(c.toString());
+        }
+        System.out.println("--------------------------");
+        Cliente cliente=cm.consultarCliente(22);
+        System.out.println(cliente.toString());
+        //cm.agregarItemRentadoACliente(22, 13, new Date(3918,03,17), new Date(3318,03,17));
+        sqlss.commit();
+        
+        
+        sqlss.close();
+
+        
+        
+    }
+
+
+}
