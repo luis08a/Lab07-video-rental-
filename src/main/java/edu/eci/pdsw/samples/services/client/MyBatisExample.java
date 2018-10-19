999f052c4f67722215a145ae80f475240f786a0c
luis <luis.moreno-a@mail.escuelaing.edu.co>
2018-10-19 15:00:37 -0500

    Binds y correción de consultar multa alquiler

--- src/main/java/edu/eci/pdsw/samples/services/client/MyBatisExample.java
+++ src/main/java/edu/eci/pdsw/samples/services/client/MyBatisExample.java
@@ -36,6 +36,9 @@ import edu.eci.pdsw.samples.entities.Cliente;
 import edu.eci.pdsw.samples.entities.Item;
 import edu.eci.pdsw.samples.entities.ItemRentado;
 import edu.eci.pdsw.samples.entities.TipoItem;
+import edu.eci.pdsw.samples.services.ExcepcionServiciosAlquiler;
+import edu.eci.pdsw.samples.services.ServiciosAlquiler;
+import edu.eci.pdsw.samples.services.ServiciosAlquilerFactory;
 
 /**
  *
@@ -67,11 +70,13 @@ public class MyBatisExample {
      * Programa principal de ejempo de uso de MyBATIS
      * @param args
      * @throws SQLException 
+     * @throws ExcepcionServiciosAlquiler 
      */
-    public static void main(String args[]) throws SQLException {
-        SqlSessionFactory sessionfact = getSqlSessionFactory();
-        SqlSession sqlss = sessionfact.openSession();
-        ClienteMapper cm=sqlss.getMapper(ClienteMapper.class);
+    public static void main(String args[]) throws SQLException, ExcepcionServiciosAlquiler {
+    	ServiciosAlquiler services = ServiciosAlquilerFactory.getInstance().getServiciosAlquiler();
+    	System.out.println(services.consultarMultaAlquiler(101, Date.valueOf("2017-03-21")));
+        //SqlSessionFactory sessionfact = getSqlSessionFactory();
+        //SqlSession sqlss = sessionfact.openSession();
         //List<Cliente> cc=cm.consultarClientes();
         //for(Cliente c:cc) {
         //	System.out.println(c.toString());
@@ -84,9 +89,9 @@ public class MyBatisExample {
         //for(ItemRentado ir: itsr) {
         //	System.out.println(ir.toString());
         //}
-        Cliente ct=new Cliente("gggg", 666, "31313","momo", "gggg@momo.index");
-        cm.insertarCliente(ct);
-        System.out.println("--------------------------");
+        //Cliente ct=new Cliente("gggg", 666, "31313","momo", "gggg@momo.index");
+        //cm.insertarCliente(ct);
+        //System.out.println("--------------------------");
         //cm.vetar(12345, estado);
         //cm.agregarItemRentadoACliente(12345, 2137559, new Date(2018,03,15), new Date(2018,03,17));
         /*TipoItem ti=new TipoItem(1,"Video");
@@ -102,8 +107,8 @@ public class MyBatisExample {
         }
         System.out.println("--------------------------");
         */
-        sqlss.commit();
-        sqlss.close();
+        //sqlss.commit();
+        //sqlss.close();
     }
 
 
