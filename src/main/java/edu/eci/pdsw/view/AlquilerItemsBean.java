c9b8ded678d69d7ed858ead8ae16f599a15dee9c
UroBs17 <michael.preciado@mail.escuelaing.edu.co>
2018-10-21 13:59:54 -0500

    avance

--- src/main/java/edu/eci/pdsw/view/AlquilerItemsBean.java
+++ src/main/java/edu/eci/pdsw/view/AlquilerItemsBean.java
@@ -1,14 +1,29 @@
 package edu.eci.pdsw.view;
 
 import com.google.inject.Inject;
+
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+
+import edu.eci.pdsw.samples.services.ExcepcionServiciosAlquiler;
 import edu.eci.pdsw.samples.services.ServiciosAlquiler;
+import edu.eci.pdsw.samples.entities.*;
+@SuppressWarnings("deprecation")
+@ManagedBean (name ="alquilerItemsBean")
+@SessionScoped
 
 public class AlquilerItemsBean extends BasePageBean{
 	private static final long serialVersionUID = 3594009161252782831L;
-
 	@Inject
-	private ServiciosAlquiler blogServices;
+	private ServiciosAlquiler sa;
+	public List<Cliente> getClientes() throws Exception {
+		try{
+			return sa.consultarClientes();
+		}catch(ExcepcionServiciosAlquiler e) {
+			throw e;
+		}
+	}
+	
 }
